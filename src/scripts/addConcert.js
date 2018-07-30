4fde4905ed728e830c011189986ca723814b7a58
mdroberts74 <mikedroberts88@gmail.com>
2018-07-30 11:03:57 -0500

    I made changes to the fields on the Add Concert Form.

--- src/scripts/addConcert.js
+++ src/scripts/addConcert.js
@@ -8,17 +8,18 @@ const addConcertObject = Object.create({}, {
     addConcertForm: {
         value: () => {
 
-            const mainDiv = document.querySelector(".main-div-container");
+            const baseDiv = document.querySelector(".base-div-container");
 
-            const $formDiv = $("<div>").attr("id", "form-div").appendTo(mainDiv)
-            const $pEl = $("<p>").appendTo($formDiv)
-            const $bookTitleLabel = $("<label>").attr("for", "book-title").text("Book Title")
-            const $bookTitleInput = $("<input>").attr("id", "book-title").attr("placeholder", "Book Title").appendTo($formDiv)
-            const $bookSumLabel = $("<label>").attr("for", "book-Sum").text("Summary")
-            const $bookSumInput = $("<input>").attr("id", "book-Sum").attr("placeholder", "Book Summary").appendTo($formDiv)
-            const $bookPagesLabel = $("<label>").attr("for", "book-pages").text("Pages")
-            const $bookPagesInput = $("<input>").attr("id", "book-pages").attr("placeholder", "Pages").appendTo($formDiv)
-            const $bookAddSubmit = $("<button>").attr("id", "bookSubmit").text("Submit Your Book").appendTo($formDiv)
+            const $concertFormDiv = $("<div>").attr("id", "concert-form-div").appendTo(baseDiv)
+            const $pEl = $("<p>").appendTo($concertFormDiv)
+            const $bandNameLabel = $("<label>").attr("for", "band-name").text("Band Name")
+            const $bandNameInput = $("<input>").attr("id", "band-name").attr("placeholder", "Band Name").appendTo($concertFormDiv)
+            const $concertDateLabel = $("<label>").attr("for", "concert-date").text("Concert Date")
+            const $concertDateInput = $("<input>").attr("type", "date").attr("id", "concert-date").attr("placeholder", "Concert Date").appendTo($concertFormDiv)
+            const $concertVenueLabel = $("<label>").attr("for", "concert-venue").text("Concert Venue")
+            const $bookPagesInput = $("<input>").attr("id", "concert-venue").attr("placeholder", "Concert Venue").appendTo($concertFormDiv)
+
+            const $bookAddSubmit = $("<button>").attr("id", "bookSubmit").text("Add Your Concert").appendTo($concertFormDiv)
 
         }
 
