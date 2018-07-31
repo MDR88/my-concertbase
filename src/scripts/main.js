38fa53586fc0f8a52ec460d251448bae60c8a8e9
mdroberts74 <mikedroberts88@gmail.com>
2018-07-31 10:48:48 -0500

    The added concerts are loaded when the home page loads.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
 //Author: Michael Roberts
 //Purpose: This is the main.js for the myConcertBase Web Application.
 const addConcert = require("./addConcert")
+const PrintToDom = require("./printToDOM")
 
 
 
 
 console.log(addConcert)
+PrintToDom.addConcertToDOM()
 addConcert.addConcertForm()
+
